4b3fe00e274adfbac144e090eb3f9907ae2271a5
Haven Shi <haiwen.shi01@gmail.com>
2016-10-17 17:23:10 -0500

    new files

---
+++ 1. Two Sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range (i+1,len(nums)):
+                if target-nums[i]==nums[j]:
+                    return [i,j]
+
+if __name__ == "__main__":
+    answer = Solution()
+    nums=[2, 7, 11, 15]
+    target = 9
+    print answer.twoSum(nums, target)
