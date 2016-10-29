7ef3c5398337a0bf450693f7cc63ed13a9154e86
Haven Shi <haiwen.shi01@gmail.com>
2016-10-29 11:23:14 -0500

    new files

---
+++ 24.py
@@ -0,0 +1,26 @@
+# take the lead
+m=2
+n=1
+
+if m < n:
+    m, n = n, m
+
+horse = [(1, 2), (2, 1), (-1, 2), (-2, 1), (-2,-1),(-1,-2),(1, -2), (2, -1)]
+queue = [(0, 0)]
+goal = {(0, 0): 0}
+
+while queue != []:
+    base = queue.pop(0)
+
+    for item in horse:
+        arrow = ((item[0] + base[0]), (item[1] + base[1]))
+        if 0 <= arrow[0] <= m and 0 <= arrow[1] <= n and (not arrow in goal):
+            goal[arrow] = goal[base] + 1
+            queue.append(arrow)
+            if arrow == (m, n):
+                break
+
+if (m, n) in goal:
+    print goal[(m, n)]
+else:
+    print -1
