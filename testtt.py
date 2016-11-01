4338b967174736ad3ab837b79efd397a62d4b11d
Haven Shi <haiwen.shi01@gmail.com>
2016-11-01 00:25:15 -0500

    new files

--- testtt.py
+++ testtt.py
@@ -1,10 +1,5 @@
-# The password generated
+# Integer solutions
 
-def pow_mod(a,n,mod):
-    res = 1
-    while n > 0 :
-        if n % 2 == 1: res = res * a % mod
-        a = a * a % mod
-        n >>= 1
-    return res
-print pow_mod(3,3,20132013)
+L=['abc','d','efg']
+
+print " ".join([x for x in L])
