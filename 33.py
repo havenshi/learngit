5409c3f3b8f7c667f0c9fd7c92614a015e7ccf35
Haven Shi <haiwen.shi01@gmail.com>
2016-12-04 19:29:44 -0600

    new files

--- 33.py
+++ 33.py
@@ -1,11 +1,14 @@
 # Big power operation
 
-def pow_mod(a,n,mod):
+a=9
+n=50
+def pow_mod(a,n,mod):  # a**50=(a**2)**25, a**25=(a**2)**12*a,
     res = 1
     while n > 0 :
-        if n % 2 == 1:
+        if n % 2 == 1:  # if n is odd, extract new_a%mod and multiply res
             res = res * a % mod
-        a = a * a % mod
-        n >>= 1
+        a = a * a % mod # 1.a=new_a**2%d
+        n >>= 1         # 2.n//2
+        print n,a,res
     return res
-print pow_mod(a,n,20132013)
+print pow_mod(a,n,7)
