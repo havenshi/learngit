d4de79d78196e83cb3d7e88dd9f44b7223e019c9
Haven Shi <haiwen.shi01@gmail.com>
2016-12-04 20:12:46 -0600

    new files

--- 33.py
+++ 33.py
@@ -9,6 +9,7 @@ def pow_mod(a,n,mod):  # a**50=(a**2)**25, a**25=(a**2)**12*a,
             res = res * a % mod
         a = a * a % mod # 1.a=new_a**2%d
         n >>= 1         # 2.n//2
-        print n,a,res
+        print n,a,res   # when n//2, new_a=a**2.then res=old_remain*one_a(as new_remain)
     return res
 print pow_mod(a,n,7)
+# a**15=a*(a**2)*(a**4)*(a**8)
