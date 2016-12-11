6b31478c3b715e328355a57687601235ab3507dc
Haven Shi <haiwen.shi01@gmail.com>
2016-12-10 22:34:01 -0600

    new files

---
+++ layer_sum.py
@@ -0,0 +1,17 @@
+LIST = [1, [2, [3, 4], 5], 6, [7, 8]]
+LAYER=1
+
+map = {}
+def layer_sum(L,layer=1):
+    for i in range(len(L)):
+        if type(L[i])==type(1):
+            if layer in map:
+                map[layer]+=L[i]
+            else:
+                map[layer]=L[i]
+        else:
+            layer_sum(L[i],layer+1)
+
+    return sum([i*map[i] for i in map.keys()])
+
+print layer_sum(LIST,LAYER)
