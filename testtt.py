3bf298137c84f3700e5081298dfe4bdcaa1bf399
Haven Shi <haiwen.shi01@gmail.com>
2016-12-28 12:09:37 -0600

    new files

--- testtt.py
+++ testtt.py
@@ -1,24 +1,46 @@
-import Tkinter as tk
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        begin=0
+        m=len(matrix)-1
+        if m<0:
+            return []
+        else:
+            n = len(matrix[0])-1
+            return self.rotate(matrix,begin,m,n)
 
-class MyApp(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        self.entry = tk.Entry(self)
-        self.entry.pack()
-        close_button = tk.Button(self, text="Close", command=self.close)
-        close_button.pack()
-        self.string = ""
+    def rotate(self,l,start,end1,end2):
+        r = []
+        if end1!=end2:
+            if start==end1 and start!=end2:
+                for y in range(start,end2+1):
+                    r.append(l[start][y])
+            else:
+                for x in range(start,end1+1):
+                  r.append(l[x][start])
+        if end1==end2:
+            if start==end1:
+                r.append(l[start][start])
+            else:
+                r=r+[l[start][start]]+[l[start][end1]]+[l[end1][start]]+[l[end1][end1]]
+        else:
+            for y in range(start,end2+1):
+                r.append(l[start][y])
+            for x in range(start+1,end1+1):
+                r.append(l[x][end2])
+            for y in range(end2-1,start-1,-1):
+                r.append(l[end1][y])
+            for x in range(end1-1,start+1-1,-1):
+                r.append(l[x][start])
+            r=r+self.rotate(l, start+1, end1-1, end2-1)
+        return r
 
-    def close(self):
-        global result
-        self.string = self.entry.get()
-        self.destroy()
-
-    def mainloop(self):
-        tk.Tk.mainloop(self)
-        return self.string
-
-print "enter a string in the GUI"
-app = MyApp()
-result = app.mainloop()
-print "you entered:", result
+if __name__ == "__main__":
+    answer=Solution()
+    print answer.spiralOrder([
+ [ 1, 2],
+ [ 3, 4 ]
+])
