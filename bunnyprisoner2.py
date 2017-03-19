188c0f09ed213a4ff13b43b2f56056ca52aa8ae5
Haven Shi <haiwen.shi01@gmail.com>
2017-03-19 14:53:03 -0500

    new files

--- bunnyprisoner2.py
+++ bunnyprisoner2.py
@@ -2,14 +2,26 @@ def answer(array):
     result = [i for i in array if i%3 ==0]
     list1 = [i for i in array if i % 3 == 1]
     list2 = [i for i in array if i % 3 == 2]
-
     list1.sort()
+    list1.reverse()
+    list2.sort()
+    list2.reverse()
+
+    result2 = []
+    pair = min(len(list1),len(list2))
+    result2 += list1[:pair] + list2[:pair]
+    remaining1 = list1[pair:]
+    remaining2 = list2[pair:]
+    triple11 = len(remaining1) / 3 * 3
+    triple22 = len(remaining2) / 3 * 3
+    result2 += remaining1[:triple11] + remaining2[:triple22]
+
+
     triple1 = len(list1)/3
     newlist1 = list1[:triple1*3]
     result += newlist1
     remain1 = list1[triple1*3:]
 
-    list2.sort()
     triple2 = len(list2) / 3
     newlist2 = list2[:triple2 * 3]
     result += newlist2
@@ -19,11 +31,20 @@ def answer(array):
     result3 = remain1[:triple3] + remain2[:triple3]
     result += result3
 
+
     result.sort()
+    result.reverse()
     answer = 0
     for i in (range(len(result))):
-        answer = answer*10 + result[::-1][i]
-    return answer
+        answer = answer*10 + result[i]
+
+    result2.sort()
+    result2.reverse()
+    answer2 = 0
+    for i in (range(len(result2))):
+        answer2 = answer2 * 10 + result2[i]
+
+    return max(answer,answer2)
 
 
-print answer([4,1,4])
+print answer([4,1,4,2,2])
