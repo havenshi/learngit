8fbb638de039dc586c6981048325aabc11489692
Haven Shi <haiwen.shi01@gmail.com>
2017-03-23 00:00:23 -0500

    new files

--- bunnyprisoner4.py
+++ bunnyprisoner4.py
@@ -1,12 +1,12 @@
 def bunny(n):
-    matrix = [[0 for j in range(n+1)] for i in range(n/2+1)]
+    matrix = [[0 for j in range(n+1)] for i in range(n/2+2)]
     matrix[1][1] = 1
-    for i in range(n/2+1):
+    for i in range(1, n/2+2):
         for j in range(i+1, n+1):
             if i == 1 or i == 2:
                 matrix[i][j] = 1
             matrix[i][j] = matrix[i][j - i] + (matrix[i - 1][j - 1] - find1(j - 1, i - 1))  # dp[n][k] = dp[n - k][k] + (dp[n - 1][k - 1] - find1[n - 1][k - 1])
-    return sum([matrix[i][n] for i in range(2, n/2+1) if matrix[i][n] > 0])
+    return sum([matrix[i][n] for i in range(2, n/2+2)])
 
 def find1(n,k):
     if n == 0 or n == 1 and k >= 2 or n == 2 and k == 2:
@@ -25,3 +25,6 @@ def find1(n,k):
         result += ((n - 1 - (start+start+(k-3))*(k-2)/2) - (start + (k-3)) + 1)/2
         start += 1
     return result
+
+
+print bunny(200)
