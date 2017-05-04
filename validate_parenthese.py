308299120c46c094392713ec36e28efb2ef3de38
Haven Shi <haiwen.shi01@gmail.com>
2017-05-04 16:35:19 -0500

    new files

--- validate_parenthese.py
+++ validate_parenthese.py
@@ -1,18 +1,25 @@
 def validate(s):
     stack = []
+    stackindex = []
     n = len(s)
     i = 0
     while i < n:
         if s[i] == '(':
             stack.append(s[i])
+            stackindex.append(i)
         if s[i] == ')':
             if stack:
                 stack.pop()
+                stackindex.pop()
             else:
                 s = s[:i] + s[i+1:]
                 n -= 1
                 continue
         i += 1
+    s = list(s)
+    s = ''.join([s[i] for i in range(n) if i not in stackindex])
+
     return s
 
-print validate('(())))((()))')
+print validate('(())((((()))')
+print validate('(())((())))')
