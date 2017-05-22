0aa53e8f75cb4794644f0b1fa594ff51b6d6e484
Haven Shi <haiwen.shi01@gmail.com>
2017-05-22 11:46:27 -0500

    new files

--- gist32.py
+++ gist32.py
@@ -1,8 +1,7 @@
 # -*- coding:utf8 -*-
-import numpy
+
 def answer(m):
     n = len(m)
-    oldl = [i for i in range(n)]
     newl = []
     for i in range(n):
         if list(set(m[i])) == [0]:
@@ -10,11 +9,12 @@ def answer(m):
     gap = len(newl)-1 # gap都是absorbing matrix, 对角线都为1. gap = 3
     for i in range(n):
         if i not in newl:
-            newl.append(i)
-    # 构造一个new matrix, 横纵坐标为[2, 3, 4, 5, 0, 1]
+            newl.append(i) # newl = [2, 3, 4, 5, 0, 1]
+
+    # 构造一个new matrix, 横纵坐标为[2, 3, 4, 5, 0, 1]，分为I块,O块，R和Q块一共四块
     matrix = [[0 for j in range(n)] for i in range(n)]
-    # I块,O块，R和Q块
-    # 求F = (I-Q)^-1
+    # 第一步求F = (I-Q)^-1
+
     # Q
     length = n-(gap+1)
     Q = [[0 for j in range(length)] for i in range(length)]
@@ -23,6 +23,7 @@ def answer(m):
             row = newl[gap+1+i]
             col = newl[gap+1+j]
             Q[i][j] = [m[row][col],sum(m[row])]# Q = [[[0, 2], [1, 2]], [[4, 9], [0, 9]]]
+
     # I
     I = [[0 for j in range(length)] for i in range(length)]
     for i in range(length):
@@ -32,11 +33,13 @@ def answer(m):
                 I[i][j] = [tmp, tmp]
             else:
                 I[i][j] = [0,tmp] # I = [[[2, 2], [0, 2]], [[0, 9], [9, 9]]]
+
     # I-Q
     IminusQ = [[0 for j in range(length)] for i in range(length)]
     for i in range(length):
         for j in range(length):
             IminusQ[i][j] = [I[i][j][0] - Q[i][j][0],I[i][j][1]] # IminusQ = [[[2, 2], [-1, 2]], [[-4, 9], [9, 9]]]
+
     # 求(I-Q)的逆矩阵F
     # 先转成整数，记录最小公倍数lcmvalue。再求逆矩阵，记录行列式的值A
     lcmvalue = IminusQ[0][0][1]
@@ -48,13 +51,15 @@ def answer(m):
     A = MatrixGetDet(IminusQ) # A = 252
     denominator = A / lcmvalue # 14
     F = [[0 for j in range(length)] for i in range(length)]
+    # 求伴随矩阵，注意先删j行，再删i列
     for i in range(length):
         for j in range(length):
-            copy = IminusQ[:]
-            copy = numpy.delete(copy, (j), axis=0)  # 注意先删j行，再删i列
-            copy = numpy.delete(copy, (i), axis=1)
+            copy = IminusQ[:j]+IminusQ[j+1:]
+            for x in range(len(copy)):
+                copy[x] = copy[x][:i]+copy[x][i+1:]
             F[i][j] = [MatrixGetDet(copy) * pow(-1,i+j), denominator] # 不用除以A，保证全部是整数。F=[[[18, 14], [9, 14]], [[8, 14], [18, 14]]]
-    # 求FR
+
+    # 第二步求FR
     R = [[0 for j in range(gap+1)] for i in range(length)]
     for i in range(length):
         for j in range(gap+1):
@@ -82,18 +87,33 @@ def answer(m):
         FR[i] /= res
     return FR
 
+# 最大公约数
 def hcf(x, y):
     if x > y:
         smaller = y
     else:
         smaller = x
-
+    hcf = 1
     for i in range(1, smaller + 1):
         if ((x % i == 0) and (y % i == 0)):
             hcf = i
-
     return hcf
 
+# 最小公倍数
+def lcm(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+    lcm = x*y
+    while (True):
+        if ((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+
+# 求行列式的值
 def MatrixGetDet(M):
     length = len(M)
     if length == 1:
@@ -127,18 +147,6 @@ def MatrixGetDet(M):
         negative += _tmp
     # return
     return positive - negative
-
-def lcm(x, y):
-    if x > y:
-        greater = x
-    else:
-        greater = y
-    while (True):
-        if ((greater % x == 0) and (greater % y == 0)):
-            lcm = greater
-            break
-        greater += 1
-    return lcm
 
 
 m= [[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
